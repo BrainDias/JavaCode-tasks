74b979f8ebb2d11352b0ae3f10d62639022402fd
Danyl Paniutchev <yankee.democratic@gmail.com>
2025-03-19 00:51:12 +0300

    Создан GeometryUtils

---
+++ GeometryUtils/src/main/java/GeometryUtils.java
@@ -0,0 +1,12 @@
+public class GeometryUtils {
+    // Преобразование из сантиметров в метры
+    public static double cmToMeters(double cm) {
+        return cm / 100;
+    }
+
+    // Сравнение двух кругов по радиусам
+    public static boolean areCirclesEqual(double radius1, double radius2) {
+        return radius1 == radius2;
+    }
+}
+
